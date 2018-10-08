9e9fd071381dd974c94903840b27d829004cd3e0
AnaHarris <vana1990@gmail.com>
2018-10-08 16:51:52 -0400

    done

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,11 @@
-def count_elements(array)
-  # code goes here
+def count_elements(arr)
+  new_hash = {}
+  arr.each do |el|
+    if new_hash[el] == nil
+      new_hash[el] = 1
+    else
+      new_hash[el] += 1
+    end
+  end
+  return new_hash
 end
- 
