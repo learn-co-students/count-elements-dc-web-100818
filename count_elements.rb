cf666c677806e0b975ba9ee016a4a9322d654553
Joseph Arias <arias.joseph.a@gmail.com>
2018-10-08 17:17:25 -0400

    Iteration and Building Hashes

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,15 @@
+require 'pry'
+
+
 def count_elements(array)
   # code goes here
+  new_hash = {}
+  array.each do |word|
+    if !new_hash[word]
+      new_hash[word] = 0
+    end
+    new_hash[word] += 1
+  end 
+  new_hash
 end
  
