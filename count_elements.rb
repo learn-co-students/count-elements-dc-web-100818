d32a83ecdcb391a89d7dc75758373a339f598306
jennyjean <jennyjean8675309@yahoo.com>
2019-05-16 10:49:58 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,17 @@
+# animals = ['tortoise', 'aye-aye', 'honey badger', 'aye-aye', 'tortoise', 'tortoise']
+# count_elements(animals)
+# => {'tortoise' => 3, 'aye-aye' => 2, 'honey badger' => 1}
+
 def count_elements(array)
-  # code goes here
+  count = {}
+
+  array.each do |animal|
+    if count[animal]
+      count[animal] += 1
+    else
+      count[animal] = 1
+    end
+  end
+
+  count
 end
- 
